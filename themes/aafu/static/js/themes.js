fd2d5904b20ab3a818e80a45a3a8eb071546d2c0
Doug7Vinicius <doug7statistic@gmail.com>
2019-07-16 22:09:23 -0400

    ajustes

--- themes/aafu/static/js/themes.js
+++ themes/aafu/static/js/themes.js
@@ -1,9 +1,9 @@
 var lightTheme = {
   mainBackground: "bg-light",
   leftBackground: "bg-white",
-  skillBackground: "bg-primary",
+  skillBackground: "bg-warning",
   mainText: "text-dark",
-  linkText: "text-primary",
+  linkText: "text-warning",
   toggleIcon: "fa-toggle-off",
   shadow: "shadowsDark"
 };
@@ -11,9 +11,9 @@ var lightTheme = {
 var darkTheme = {
   mainBackground: "bg-black",
   leftBackground: "bg-alt-black",
-  skillBackground: "bg-warning",
+  skillBackground: "bg-primary",
   mainText: "text-white",
-  linkText: "text-warning",
+  linkText: "text-primary",
   toggleIcon: "fa-toggle-on",
   shadow: "shadowsLight"
 };
